c4e82dcc69d57439f972c4c5c572057488a060c8
leandroluizpereira <leandro.2306@hotmail.com>
2022-06-12 17:48:50 -0300

    save

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let pergunta = {
-    titulo: 'Gato' ,
-    alternativas:['Gat','Cat','Gate','Dog'],
+    titulo: 'Gato',
+    alternativas: ['Gat','Cat','Gate','Dog'],
     correta: 1
 };
 
@@ -8,12 +8,26 @@ function mostraquestao(q){
     let titleDiv = document.getElementById('titulo');
     titleDiv.textContent = q.titulo;
 
-    let alts = document.querySelectorAll('alternativa');
+    let alts = document.querySelectorAll('.alternativa');
 
  alts.forEach(function(element,index){
         element.textContent = q.alternativas[index];
+        element.addEventListener('click',function(){
+             if(index == q.correta){
+                 console.log("Acertou!");
+             }
+             else{
+                 console.log("Errou!");
+             }
+        })
     });
 }
 
 mostraquestao(pergunta);
 
+
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click',function(){
+ console.log("clique");
+});
